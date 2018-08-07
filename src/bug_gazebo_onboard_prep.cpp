2732ad7f91e4d92a5740bf196f2bdd05d6b0d2d3
knmcguire <k.n.mcguire@tudelft.nl>
2018-08-07 14:51:41 +0200

    [msgs] got heading of gazebo

--- src/bug_gazebo_onboard_prep.cpp
+++ src/bug_gazebo_onboard_prep.cpp
@@ -9,13 +9,15 @@
 
 #include "ros/ros.h"
 #include "ros/time.h"
-
+#include <tf/transform_datatypes.h>
 
 #include "std_msgs/String.h"
 #include "sensor_msgs/LaserScan.h"
 #include "sensor_msgs/Range.h"
 #include "hector_uav_msgs/EnableMotors.h"
 #include "geometry_msgs/Twist.h"
+#include "geometry_msgs/PoseStamped.h"
+
 
 #include <sstream>
 
@@ -29,6 +31,7 @@ float front_range;
 float right_range;
 float left_range;
 float height;
+float heading;
 
 void frontRangeCB(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
@@ -50,6 +53,17 @@ void sonarHeightCB(const sensor_msgs::Range::ConstPtr& msg)
 	height = msg->range;
 }
 
+void poseCB(const geometry_msgs::PoseStamped::ConstPtr& msg)
+{
+	tf::Quaternion q(msg->pose.orientation.x, msg->pose.orientation.y, msg->pose.orientation.z, msg->pose.orientation.w);
+
+	tf::Matrix3x3 mat(q);
+	double yaw, pitch, roll;
+
+	mat.getEulerYPR(yaw, pitch, roll);
+	heading = yaw;
+
+}
 
 int main(int argc, char **argv)
 {
@@ -65,6 +79,7 @@ int main(int argc, char **argv)
 	ros::Subscriber sub_rightrange = n.subscribe("/multi_ranger/right_range_sensor_value", 1000, rightRangeCB);
 	ros::Subscriber sub_leftrange = n.subscribe("/multi_ranger/left_range_sensor_value", 1000, leftRangeCB);
 	ros::Subscriber sub_sonarheight = n.subscribe("/sonar_height", 1000, sonarHeightCB);
+	ros::Subscriber sub_pose = n.subscribe("/ground_truth_to_tf/pose", 1000, poseCB);
 
 
 	// Publish to gazebo controller
@@ -101,10 +116,13 @@ int main(int argc, char **argv)
 			//testRange(front_range, right_range, left_range);
 			float vel_x;
 			float vel_y;
-			wall_follower(&vel_x, &vel_y, front_range,  right_range, 0,  1);
+			float vel_w;
+
+			wall_follower(&vel_x, &vel_y, &vel_w, front_range,  right_range, heading,  1);
 
 			twist_msg.linear.x = vel_x;
 			twist_msg.linear.y = vel_y;
+			twist_msg.angular.z = vel_w;
 			twist_msg.linear.z = 0.0;
 		}
 
