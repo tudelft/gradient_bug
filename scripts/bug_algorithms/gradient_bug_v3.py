[gradientbug v3] added gradient search and multiple bug

scripts/bug_algorithms/gradient_bug_v3.py
@@ -73,7 +73,12 @@ class GradientBugController:
     overwrite_and_reverse_direction = False
     not_out_of_the_woods = True
     
-    angle_goal = 0;
+    angle_goal = 0
+    goal_angle_dir = -1
+    correct_heading_array = []
+    rssi_sample_reset =  False
+    prev_rssi = 0;
+    saved_pose_sample = PoseStamped()
     
     def init(self,new_ref_distance_from_wall,max_speed_ref = 0.2, max_rate_ref = 0.5, angle_goal_init=-2.27):
         self.ref_distance_from_wall = new_ref_distance_from_wall
@@ -100,6 +105,9 @@ class GradientBugController:
         self.loop_angle=0
         self.angle_goal = angle_goal_init
         self.goal_angle_dir = -1
+        
+        self.correct_heading_array = [0, 0, 0, 0, 0, 0, 0, 0]
+        self.rssi_sample_reset = False
 
     def take_off(self):
         twist = Twist()
@@ -165,6 +173,66 @@ class GradientBugController:
         return state
     
     
+    def fillHeadingArray(self,rssi_heading, diff_rssi, max_meters):
+        
+        #heading array of action choices
+        heading_array = [-135.0, -90.0, -45.0, 0.0, 45.0, 90.0, 135.0, 180.0]
+        rssi_heading_deg = math.degrees(rssi_heading);
+        #print(rssi_heading_deg)
+        
+        for it in range(8):
+            #fill array based on heading and rssi heading
+            if ((rssi_heading_deg>=heading_array[it]-22.5) and (rssi_heading_deg<heading_array[it]+22.5) and it is not 7) or \
+            (it is 7 and ((rssi_heading_deg>=heading_array[it]-22.5) or (rssi_heading_deg<-135.0 - 22.5))):
+                
+                temp_value_forward = self.correct_heading_array[it]
+                temp_value_backward =  self.correct_heading_array[(it + 4) % 8]
+                
+                #If gradient is good, increment the array corresponding to the current heading and
+                #   decrement the exact opposite
+                if (diff_rssi> 0):
+                    self.correct_heading_array[it]=temp_value_forward + 1
+                    if (temp_value_backward >0):
+                        self.correct_heading_array[(it+4)%8]=temp_value_backward - 1
+                # if gradient is bad, decrement the array corresponding to the current heading and 
+                #   increment the exact opposite
+                elif diff_rssi<0:
+                    self.correct_heading_array[(it+4)%8] = temp_value_backward + 1
+                    if(temp_value_forward>0):
+                        self.correct_heading_array[it] = temp_value_forward - 1
+                        
+                        
+        # degrading function
+        #    If one of the arrays goes over maximum amount of points (meters), then decrement all values                  
+                        
+        if max(self.correct_heading_array)>max_meters:
+            for it in range(8):
+                if self.correct_heading_array[it]>0:
+                    self.correct_heading_array[it]=self.correct_heading_array[it]-1
+                    
+                    
+        # Calculate heading where the beacon might be
+        count = 0
+        y_part = 0
+        x_part = 0
+        
+        for it in range(8):
+            if(self.correct_heading_array[it]>0):
+                x_part += self.correct_heading_array[it]*math.cos(heading_array[it]*math.pi/180.0)
+                y_part += self.correct_heading_array[it]*math.sin(heading_array[it]*math.pi/180.0)
+                count = count + self.correct_heading_array[it]
+                
+        wanted_angle_return = 0
+        if count is not 0:
+            wanted_angle_return = math.atan2(y_part/count,x_part/count)
+    
+    
+        #print("array",self.correct_heading_array)
+        #print("array",wanted_angle_return)
+        return wanted_angle_return
+
+        
+    
 
 
         
@@ -216,8 +284,8 @@ class GradientBugController:
         # ROTATE_TO_GOAL
         # MOVE_OUT_OF_WAY
 
-        print self.overwrite_and_reverse_direction
-        print self.state
+        #print self.overwrite_and_reverse_direction
+        #print self.state
         #################### STATE TRANSITIONS#####################
         # Forward
         if self.state == "FORWARD":
@@ -259,7 +327,7 @@ class GradientBugController:
 
 
                 # GRADIENTBUG: implement reinitialize heading array!!
-                
+                self.correct_heading_array = [0,0,0,0,0,0,0,0]
                 #Go to wall_following
                 self.state = self.transition("WALL_FOLLOWING")
                 self.already_reversed_direction = False
@@ -276,7 +344,7 @@ class GradientBugController:
             temp_loop_distance = np.sqrt(rel_x_loop*rel_x_loop+rel_y_loop*rel_y_loop)
             self.loop_angle =float(temp_loop_angle[0])
             
-                                # Check if the robot has moved behing him
+             # Check if the robot has moved behing him
             if (abs(wraptopi(self.angle_goal+np.pi-self.loop_angle))<1.0)and temp_loop_distance>1:
                 self.overwrite_and_reverse_direction = True
                 
@@ -285,10 +353,10 @@ class GradientBugController:
             if bot_is_close and priority is False:
                 #GRADIENTBUG: See if bearing goal needs flipping or something else
                 if outbound:
-                    if (other_goal_angle < 0 and self.angle_goal < 0) or (other_goal_angle>0 and self.angle_goal>0):
-                       self.angle_goal = -1* self.angle_goal
-                       self.goal_angle_dir = wraptopi(current_heading-self.angle_goal)
-
+                    #if (other_goal_angle < 0 and self.angle_goal < 0) or (other_goal_angle>0 and self.angle_goal>0):
+                     #  self.angle_goal = -1* self.angle_goal
+                     #  self.goal_angle_dir = wraptopi(current_heading-self.angle_goal)
+                    self.angle_goal = self.angle_goal
                 if bot_is_really_close:
                     self.state = self.transition("MOVE_OUT_OF_WAY")
                 pass
@@ -309,7 +377,26 @@ class GradientBugController:
                     # Goto rotate_to_goal
                     self.state = self.transition("ROTATE_TO_GOAL")
                     pass
+            
+            if self.state_WF is "WALL_FOLLOWING":
+                #reset sample gattering
+                if self.rssi_sample_reset:
+                    self.saved_pose_sample = deepcopy(odometry)
+                    self.rssi_sample_reset = False
+                    self.prev_rssi = rssi_to_tower
                 
+                rel_x_sample = odometry.pose.position.x- self.saved_pose_sample.pose.position.x 
+                rel_y_sample = odometry.pose.position.y - self.saved_pose_sample.pose.position.y  
+                distance = math.sqrt(rel_x_sample*rel_x_sample + rel_y_sample*rel_y_sample)
+                
+                if distance > 1.0:
+                    self.rssi_sample_reset = True
+                    heading_rssi = current_heading
+                    diff_rssi =  self.prev_rssi - rssi_to_tower
+                    #print("diff rssi", diff_rssi)
+                    
+                    if outbound is False:
+                        self.angle_goal = self.fillHeadingArray(heading_rssi, diff_rssi, 5)
             #GRADIENT implement the gradient search during wallfollowing here!!
             
         # Rotate to Goal