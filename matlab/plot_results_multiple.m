754607ef17e809d0ccd12d89e69ef3d31ae02c11
knmcguire <k.n.mcguire@tudelft.nl>
2019-02-17 15:20:10 +0100

    latest changes

--- matlab/plot_results_multiple.m
+++ matlab/plot_results_multiple.m
@@ -18,19 +18,28 @@ testname = strcat(dirname,'/test_20190124_084420'); % 6 agents
 testname = strcat(dirname,'/test_20190130_232402'); % 6 agents
 testname = strcat(dirname,'/test_20190131_165220'); % 6 agents
 testname = strcat(dirname,'/test_20190201_215121'); % 6 agents
+testname = strcat(dirname,'/test_20190203_120405'); % 2 agents
+
+testname = strcat(dirname,'/test_20190204_153502'); % 2 agents
+testname = strcat(dirname,'/test_20190204_225154'); % 6 agents
+testname = strcat(dirname,'/test_20190210_154859'); % 8 agents
+testname = strcat(dirname,'/test_20190211_203758'); % 4 agents
+testname = strcat(dirname,'/test_20190213_091903'); % 10 agents
 
 
 
-amount_of_agents = 6;
-amount_of_environments = 99;
+
+
+amount_of_agents = 10;
+amount_of_environments = 78
 
 do_plot = false;
 
-time_treshold = 3900:2500:28900;false
-time_treshold = 12000;%28900;
+time_treshold = 3900:2500:28900;
+time_treshold = 11900 %9000;%28900;
 size_threshold = size(time_treshold,2);
 map = jet(size_threshold);
-id_color_map = [1 0 0; 0 1 0; 0 0 1];
+id_color_map = [1 0 0; 0 1 0; 0 0 1; 0.5 0.5 0; 0 0.5 0.5; 0.3 0.3 0.3; 0.5 0 0; 0 0.5 0; 0 0 0.5];
 
 coverage = [];
 
@@ -54,23 +63,23 @@ for itt = 1:size_threshold
         end
         
         
-        
-        %calculate which part of the environment is accessable
-        filename = strcat(testname,'/environment',num2str(it));
-        img=255-imread(strcat(filename,'/environment.png'));
-        
-        img_bin = im2bw(img);
-        CC=bwconncomp(img_bin);
-        
-        linearInd = sub2ind([200,200],100,100);
-        index = 0;
-        for it_temp=1:size(CC.PixelIdxList,1)
-            if ismember(linearInd,CC.PixelIdxList{it_temp})
-                index=it_temp;
-                break
-            end
-        end
-        accessable_area_percentage = size(CC.PixelIdxList{index},1)/(200*200);
+        accessable_area_percentage =1;
+%         %calculate which part of the environment is accessable
+%         filename = strcat(testname,'/environment',num2str(it));
+%         img=255-imread(strcat(filename,'/environment.png'));
+%         
+%         img_bin = im2bw(img);
+%         CC=bwconncomp(img_bin);
+%         
+%         linearInd = sub2ind([200,200],100,100);
+%         index = 0;
+%         for it_temp=1:size(CC.PixelIdxList,1)
+%             if ismember(linearInd,CC.PixelIdxList{it_temp})
+%                 index=it_temp;
+%                 break
+%             end
+%         end
+%         accessable_area_percentage = size(CC.PixelIdxList{index},1)/(200*200);
         
         
         for itn = 1:amount_of_agents
@@ -84,9 +93,11 @@ for itt = 1:size_threshold
             else
                 trajectory = importdata(strcat(filename,'/trajectory',num2str(itn),'.txt'),',');
             end
+            
+            
             for itl=3099:size(trajectory,1)
                 
-                if sqrt((trajectory(itl,1))^2+(trajectory(itl,2))^2)<2
+                if sqrt((trajectory(itl,1))^2+(trajectory(itl,2))^2)<2.5
                     
                     trajectory(itl+1:size(trajectory,1),:)=[];
                     break
@@ -95,7 +106,6 @@ for itt = 1:size_threshold
             end
             
             if size(trajectory,1)>time_treshold(itt)
-                
                 
                 made_it(it+1,itn) = 0;
                 
@@ -119,8 +129,13 @@ for itt = 1:size_threshold
             xy(end,:)=xy(end-1,:);
             occMap_agent = robotics.BinaryOccupancyGrid(20,20,1);
             setOccupancy(occMap_agent,xy,1);
-            OccMat = occupancyMatrix(occMap_agent);
-            coverage_per_agent(it+1,itn) = sum(OccMat(:) == 1)/(numel(OccMat)*accessable_area_percentage);
+            OccMat_agent = occupancyMatrix(occMap_agent);
+            OccMat_agent_all(:,:,itn) = OccMat_agent;
+            coverage_per_agent(it+1,itn) = sum(OccMat_agent(:) == 1)/(numel(OccMat_agent)*accessable_area_percentage);
+            
+            
+%             figure(5),show(occMap_agent),title(num2str(itn))
+%             keyboard
             
             setOccupancy(occMap,xy,1)
             
@@ -133,11 +148,33 @@ for itt = 1:size_threshold
             
         end
         
+        %check uniqueness coverage per agent
+        unique_coverage = zeros(size(OccMat_agent_all(:,:,1)));
+        for itn = 1:amount_of_agents
+            for itx = 1:size(OccMat_agent_all,1)
+                for ity = 1:size(OccMat_agent_all,2)
+                    
+                    OccMat_agent_array = OccMat_agent_all(itx,ity,:);
+                    if  OccMat_agent_all(itx,ity,itn) == true && numel(find(OccMat_agent_array==true))==1
+                        unique_coverage(itx,ity) =1;
+                    else
+                        unique_coverage(itx,ity) =0;
+                        
+                    end
+                    
+                end
+            end
+            unique_coverage_per_agent(itn) = sum(unique_coverage(:)==1)/numel(unique_coverage);
+        end
+        unique_coverage_per_agent_tot(it+1) = mean(unique_coverage_per_agent);
         
         occMat = occupancyMatrix(occMap);
         occMat_return = occupancyMatrix(occMap_return);
         coverage_tot_return(it+1,1) = sum(occMat(:) == 1)/(numel(occMat)*accessable_area_percentage);
         coverage_tot_return(it+1,2) = sum(occMat_return(:) == 1)/(numel(occMat_return)*accessable_area_percentage);
+        
+
+        
         
         
         made_it(it+1,:)
@@ -156,10 +193,11 @@ for itt = 1:size_threshold
     end
     
     sum_made_it(:,itt) = sum(made_it,2);
-    %     figure(2),histogram(sum_made_it,'facecolor',map(1,:),'facealpha',.5,'edgecolor','none'), hold on
-    %     keyboard
+%          figure(2),histogram(sum_made_it,'facecolor',map(1,:),'facealpha',.5,'edgecolor','none'), hold on
+        
     
 end
+
 
 
 
@@ -180,9 +218,9 @@ ylabel('percentage agent returned')
 
 figure(3)
 
-average_coverage_per_agent = mean(coverage_per_agent,2)
+average_coverage_per_agent = mean(coverage_per_agent,2);
 
-bar(mean([coverage_tot_return, average_coverage_per_agent]))
+bar(mean([coverage_tot_return, average_coverage_per_agent, unique_coverage_per_agent_tot']))
 
 
 % histdata = [];false
@@ -205,7 +243,7 @@ bar(mean([coverage_tot_return, average_coverage_per_agent]))
 keyboard
 %% look at environment in detail
 close all
-check_environment_number = 2
+check_environment_number = 16
 
 filename = strcat(testname,'/environment',num2str(check_environment_number));
 img=255-imread(strcat(filename,'/environment.png'));
@@ -217,8 +255,9 @@ yImg = linspace(-10,10, size(img, 1));
 image(xImg, yImg, img, 'CDataMapping', 'scaled');
 colormap(gray(256))
 
+for itn=1:amount_of_agents
 
-trajectory = importdata(strcat(filename,'/trajectory',num2str(1),'.txt'),',');
+trajectory = importdata(strcat(filename,'/trajectory',num2str(itn),'.txt'),',');
 
 %
 %  for itl=3099:size(trajectory,1)
@@ -235,8 +274,9 @@ plot(trajectory(3099,2),trajectory(3099,1),'o','Color',id_color_map(itn,:),'Mark
 plot(trajectory(3099:end,2),trajectory(3099:end,1),'Color',id_color_map(itn,:),'LineWidth',2)
 
 plot(trajectory(3099,2),trajectory(3099,1),'o','Color',id_color_map(itn,:),'MarkerSize',20)
-plot(8,8,'m*')
+plot(0,0,'m*')
 
+end
 
 % img_bin = im2bw(img);
 % CC=bwconncomp(img_bin);
